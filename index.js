2c17872c493f8bd4500e2e5e007c40445935062d
Edris_Azimi <Edris.azimi.pro@gmail.com>
2021-10-31 18:53:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+// Test 1
+function writeCards(name, string) {
+    const newArray = [];
+    for (let i = 0; i < name.length; i++) {
+      newArray.push(`Thank you, ${name[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+  }
+  
+  // Test 2 and 3
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number--);
+    }
+  }
+  
